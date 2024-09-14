ca36dce7cb0ec2af1d27572117835fb610dc7002
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-09-14 21:42:32 +1000

    Fix that one test

--- worldstate/src/commonTest/kotlin/com/fable/EntityTest.kt
+++ worldstate/src/commonTest/kotlin/com/fable/EntityTest.kt
@@ -2,12 +2,14 @@ package com.fable
 
 import com.fable.entity.Entity
 import com.fable.event.Event
+import com.fable.event.findEvent
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class EntityTest {
 
-    @Test fun `test entity`() {
+    @Test
+    fun `test entity`() {
         val entity = ConcreteEntity()
         (1..100).forEach { t -> entity.addEvent(IntegerEvent(t)) }
         entity.addEvent(BooleanEvent(true))
